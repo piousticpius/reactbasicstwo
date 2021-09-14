5536b642d3835e36a6c1e86521e5b5b410c4abbf
piousticpius <piousticpius@gmail.com>
2021-09-14 04:34:18 +0100

    reactbasicsstep1

---
+++ src/containers/ProductComponent.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+const ProductComponent=()=>{
+    const products=useSelector((state)=>state.allProducts.products);
+    const renderList=products.map((product)=>{
+        const {id,title,image,price,category}=product;
+        return(
+            <div className='four wide column' key={id}>
+                <div className='ui link cards'>        
+                    <div className='card'> 
+                        <div className='image'>
+                        <img src={image} alt={title} />
+                        </div>       
+                            
+                                 <div className='content'>
+                                      <div className='header'>{title}</div>   
+                                      <div className='meta price'>${price}</div>   
+                                      <div className='meta'>{category}</div>   
+                 
+                                                </div>
+                                            </div>
+                                    </div>
+                             </div>
+        );
+        
+    })
+    return (
+        <h1>{renderList}</h1>
+        );
+    // const{id,title}=products[0];
+    
+};
+export default ProductComponent
---
+++ src/containers/ProductDetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const ProductLDetail=()=>{
+    return(
+        <div>
+        <h1>ProductLDetail</h1>
+        </div>
+    );
+};
+export default ProductLDetail
